89e5bf0b5021da1c46fec4d2777701041439e4a9
Rafael Oliveira <rafaeloliveira0019@hotmail.com>
2021-08-26 17:44:22 -0300

    Criado conexão com BD

---
+++ src/config/database.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'admin',
+    password: 'admin',
+    database: 'poup',
+    entities: [__dirname + './../**/entity/*.entity.{js,ts}'],
+    synchronize: false,
+};
