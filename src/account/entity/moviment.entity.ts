dbf62c78c3a958a296eba9d4d1ab52a2ebf0b7a7
Rafael Oliveira <rafaeloliveira0019@hotmail.com>
2021-11-15 13:55:48 -0300

    Criado entidade de movimentação

---
+++ src/account/entity/moviment.entity.ts
@@ -0,0 +1,28 @@
+import {BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import { v4 as uuid } from 'uuid';
+import {ModalityEntity} from "./modality.entity";
+
+@Entity({schema: "cad", name: "movimento"})
+export class MovimentEntity extends BaseEntity {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column({name: "valor", type: "numeric"})
+    valor: number
+
+    @Column({name: "data", type: "date"})
+    data: string
+
+    @Column({name: "modalidade_id", type: "varchar"})
+    modalidade_id: string
+
+    @ManyToOne(type => ModalityEntity, modalidade => modalidade.movimento)
+    @JoinColumn({name: "modalidade_id", referencedColumnName: "id"})
+    modalidade: ModalityEntity
+
+    constructor() {
+        super();
+        this.id = uuid()
+    }
+}
