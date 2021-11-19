042b09a8b09a8c14a0d37ffa399f908e2f2abce5
Rafael Oliveira <rafaeloliveira0019@hotmail.com>
2021-11-18 23:48:46 -0300

    Criado repositório de conta

---
+++ src/account/repository/account.repository.ts
@@ -0,0 +1,8 @@
+import {InjectRepository} from "@nestjs/typeorm";
+import {AccountEntity} from "../entity/account.entity";
+
+export class AccountRepository {
+    constructor(@InjectRepository(AccountEntity) account: AccountEntity) {
+
+    }
+}
