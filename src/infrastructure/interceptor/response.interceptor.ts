2359dd275bd67f4b820d2ee791d6cf5d3b376da5
Rafael Oliveira <rafaeloliveira0019@hotmail.com>
2021-11-22 20:51:07 -0300

    Criado interceptador de requet's para montar response

---
+++ src/infrastructure/interceptor/response.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, map, Observable } from 'rxjs';
+import { convertReponseCorrect, convertResponseError } from '../util/response.convert';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Promise<Observable<any>> {
+    const { statusCode } = await context.switchToHttp().getResponse();
+
+    return next.handle().pipe(
+      map((data) => convertReponseCorrect(statusCode, data)),
+
+      catchError(async (error) => convertResponseError(error.status, error.message)),
+    );
+  }
+}
