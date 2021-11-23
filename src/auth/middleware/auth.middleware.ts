feafde1eecbb499cfe301e2da14c56afb183b228
Rafael Oliveira <rafaeloliveira0019@hotmail.com>
2021-11-22 22:34:37 -0300

    Criado exception personalizada para respose

--- src/auth/middleware/auth.middleware.ts
+++ src/auth/middleware/auth.middleware.ts
@@ -4,23 +4,27 @@ import {
   Inject,
   NestMiddleware,
 } from '@nestjs/common';
-import { MessageException} from "../../infrastructure/shared/enum/message-exception";
+import { MessageException } from '../../infrastructure/shared/enum/message-exception';
 import { AuthService } from '../service/auth.service';
+import { isEmpty } from '@nestjs/common/utils/shared.utils';
 
 export class AuthMiddleware implements NestMiddleware {
   constructor(@Inject(AuthService) private readonly authService: AuthService) {}
 
   use(req: any, res: any, next: () => void): any {
-    console.log(req.headers.authorization.search('Bearer'));
-
-    if (
-      !req.headers.authorization ||
-      req.headers.authorization.search('Bearer ') == -1
-    ) {
+    if (!req.headers.authorization) {
       throw new HttpException(
         MessageException.NOT_AUTHENTICATED,
         HttpStatus.UNAUTHORIZED,
       );
+    }
+    if (!isEmpty(req.headers.authorization)) {
+      if (req.headers.authorization.search('Bearer ') == -1) {
+        throw new HttpException(
+          MessageException.NOT_AUTHENTICATED,
+          HttpStatus.UNAUTHORIZED,
+        );
+      }
     }
     this.authService.decodeToken(
       req.headers.authorization.replace('Bearer ', ''),
